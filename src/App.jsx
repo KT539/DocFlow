8edabafb3d953e49cce40a76060fe44ec3e92dc2
Kilian Testard <pc47xiy@eduvaud.ch>
2026-04-28 09:28:35 +0200

    feat: page new_flows + navbar collapsing

--- src/App.jsx
+++ src/App.jsx
@@ -10,16 +10,19 @@
 import { useState } from 'react';
 import Nav from './nav.jsx';
 import Flows from './pages/flows.jsx';
+import NewFlows from './pages/new_flows.jsx';
 import Settings from './pages/settings.jsx';
 
 
 export default function App() {
   const [currentPage, setCurrentPage] = useState('flows');
+  const [navCollapsed, setNavCollapsed] = useState(false);
 
   // switch case to render the pages
   const renderPage = () => {
     switch (currentPage) {
       case 'flows': return <Flows />;
+      case 'new_flows': return <NewFlows />;
       case 'settings': return <Settings />;
       default: return <Flows />;
     }
@@ -27,7 +30,12 @@ export default function App() {
 
   return (
     <div className="flex h-screen w-full bg-gray-200">
-      <Nav setCurrentPage={setCurrentPage} activePage={currentPage} />
+      <Nav 
+        setCurrentPage={setCurrentPage} 
+        activePage={currentPage} 
+        collapsed={navCollapsed}
+        setCollapsed={setNavCollapsed}
+      />
       <main className="flex-1 p-6">
         {renderPage()}
       </main>
--- src/pages/new_flows.jsx
+++ src/pages/new_flows.jsx
@@ -5,3 +5,13 @@
  * @project_lead    Pascal Hurni
  * @last_modified   27-04-2026
  */
+
+
+export default function NewFlows() {
+    return (
+        <div className="min-h-screen bg-gray-200 text-black flex flex-col p-8">
+            <h1 className="text-4xl font-bold text-center mt-4">Nouveau Flows</h1>
+            <h3 className="text-gray-600 text-lg text-center mt-3">to do</h3>
+        </div>
+    );
+}
